f842a1a3e778a430f575fd1f37896a7c3ae4f846
Alejandro <aesbetancourt@outlook.com>
2019-05-26 03:30:12 -0400

    Improved visualization

--- src/graph.pl
+++ src/graph.pl
@@ -15,37 +15,110 @@ __DATA__
     %= javascript 'https://rawgit.com/chen0040/js-graph-algorithms/master/third-party-libs/vis/vis.js'
     %= javascript 'https://rawgit.com/chen0040/js-graph-algorithms/master/src/jsgraphs.js'
     %= stylesheet 'https://rawgit.com/chen0040/js-graph-algorithms/master/third-party-libs/vis/vis.css'
+    %= stylesheet 'https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap'
+    %= stylesheet 'https://fonts.googleapis.com/css?family=Rubik&display=swap'
     %= stylesheet begin
+        body{
+            background-image: url("https://images4.alphacoders.com/786/thumb-1920-786616.png");
+        }
+        h1, h3{
+            font-family: 'Press Start 2P', cursive;
+            color: #BAB3B3;
+        }
         textarea {
             resize: none;
+            background: transparent;
+            border: none;
+            width: 400px ;
+            outline: none;
+            color: white;
+            font-size: 16px;
+            height: 180px;
+        }
+        hr{
+            height: 1px;
+            background-color: #0378A6;
+            border: none;
+        }
+        label{
+            color: white;
+            font-family: 'Rubik', sans-serif;
+        }
+        input{
+            background: transparent;
+            border: none;
+            border-bottom: 1px solid #fff;
+            height: 40px;
+            outline: none;
+            color: white;
+            font-size: 16px;
+        }
+        p{
+            color: white;
+            font-family: 'Rubik', sans-serif;
+            font-size: 12px;
         }
         #inputs{
             position: relative;
             width: 400px;
-            border-style: solid;
             height: 95vh;
             top: 2px;
             padding-left: 20px;
         }
         #mynetwork{
-            border-style: solid;
+            /*border-style: solid;*/
             position: absolute;
             top: 10px;
             left: 30%;
             height: 95vh;
             width: 123vh;
         }
+        #renderbtn{
+            font-family: 'Press Start 2P', cursive;
+            position: relative;
+            margin-top: 5px;
+            left: 35%;
+            height: 20px;
+            border: none;
+            outline: none;
+            border-radius: 2px;
+            background-color: #0378A6;
+            font-size: 10px;
+            cursor: pointer;
+        }
     % end
 %end
   <body>
     <%= tag div => (id => 'inputs') => begin %>
-        <h1>Grafos dirigidos</h1>
-        %= input_tag 'nodes', placeholder => '#Nodo', id => 'nodes'
-        %= input_tag 'edges', placeholder => '#Edges', id => 'edges'
-        %= input_tag 'start', placeholder => 'Start', id => 'start'
-        %= submit_button 'Generar', id => 'render', onclick => 'renderGraph()'
+        <h1>Grafos Dirigidos</h1>
+        %= tag 'hr'
+        <h3>Datos del Grafo</h3>
+        %= form_for login => begin
+            %= label_for nodes => 'Cantidad de nodos:'
+            %= tag 'br'
+            %= text_field 'nodes', placeholder => 'i.e: 3', id => 'nodes'
+            %= tag 'br'
+            %= label_for edges => 'Patrón de aristas:'
+            %= tag 'br'
+            %= text_field 'edges', placeholder => 'i.e: 0-1-2 1-2-3 2-0-3', id => 'edges'
+            %= tag 'br'
+            %= tag 'hr'
+            <h3>Dijkstra</h3>
+            %= label_for start => 'Nodo Inicial'
+            %= tag 'br'
+            %= text_field 'start', placeholder => 'Inicio', id => 'start'
+        % end
+        %= tag 'hr'
+        %= submit_button 'Generar', id => 'renderbtn', onclick => 'renderGraph()'
         %= tag 'br'
-        %= text_area 'dijkstra', cols => 40, rows => 20, id => 'dijkstra'
+        %= tag 'br'
+        %= text_area 'dijkstra', cols => 45, rows => 10, id => 'dijkstra', placeholder => 'Cálculo del Algoritmo'
+        <h3>Instrucciones</h3>
+        %= tag p => begin
+            Ingrese numero de nodos del grafo, luego ingrese un patrón de aristas, dicho patron funciona
+            de la siguiente manera: "inicio-destino-peso inicio-destino-peso", i.e: 0-1-2 1-2-3 2-0-3.
+            Para el algoritmo de Dijkstra se debe ingresar el nodo inicial, sino solo se generará el grafo.
+        % end
     <% end %>
     %= tag 'div', id => 'mynetwork'
 
