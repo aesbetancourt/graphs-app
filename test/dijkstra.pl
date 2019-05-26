ef12f97526ac52c8ee83eff0b106f8ec3336d3fa
Alejandro <aesbetancourt@outlook.com>
2019-05-26 03:56:38 -0400

    Only for testing purposes

--- test/dijkstra.pl
+++ test/dijkstra.pl
@@ -5,11 +5,12 @@ use Graph::Dijkstra;
 my $graph = Graph::Dijkstra->new( {edgedefault=>'directed'} );
 
 #SET methods to create graph nodes and edges "manually"
-$graph->node( {id=>0, label=>'node 0'} );   #create or update existing node.  id must be a simple scalar.
-$graph->node( {id=>1, label=>'node 1'} );   #label is optional and should be string
+
+$graph->node( {id=>0, label=>'node 0'} );
+$graph->node( {id=>1, label=>'node 1'} );
 $graph->node( {id=>2, label=>'node 2'} );
 $graph->node( {id=>3, label=>'node 3'} );
-$graph->edge( {sourceID=>0, targetID=>1, weight=>2} );  #create or update edge between sourceID and targetID;  weight (cost) must be > 0
+$graph->edge( {sourceID=>0, targetID=>1, weight=>2} );
 $graph->edge( {sourceID=>1, targetID=>2, weight=>9} );
 $graph->edge( {sourceID=>1, targetID=>3, weight=>4} );
 $graph->edge( {sourceID=>3, targetID=>2, weight=>1} );
@@ -21,7 +22,7 @@ use Data::Dumper;
 my %Solution = ();
 
 #shortest path to farthest node from origin node
-%Solution = ( originID=>0, destinationID=>2 );
+%Solution = ( originID=>1 , destinationID=>2 );
 if ( my $pathCost = $graph->shortestPath(\%Solution) ) {
     print "Solution path from originID $Solution{originID} to destinationID $Solution{destinationID} at weight (cost) $Solution{weight}\n";
     foreach my $edgeHref (@{$Solution{edges}}) {
